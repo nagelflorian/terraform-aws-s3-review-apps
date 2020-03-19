f2aa8747f832dcf4bc63a6b6d6cd43b5c0ff1476
Florian Nagel <nagelflorian@users.noreply.github.com>
2020-03-19 19:37:18 +0100

    Update E2E Test Setup (#4)

--- tests/main_test.go
+++ tests/main_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"os"
 	"testing"
 
 	"github.com/google/uuid"
@@ -11,12 +12,25 @@ import (
 func TestReviewAppResources(t *testing.T) {
 	t.Parallel()
 
-	randomBucketName := uuid.New().String()
+	bucketName := uuid.New().String()[0:12]
+	domainName := os.Getenv("DOMAIN_NAME")
+	route53ZoneID := os.Getenv("ROUTE_53_ROUTE_ID")
+
+	if domainName == "" {
+		t.Skip("skipping test; $DOMAIN_NAME not set")
+	}
+	if route53ZoneID == "" {
+		t.Skip("skipping test; $ROUTE_53_ROUTE_ID not set")
+	}
 
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../",
 		Vars: map[string]interface{}{
-			"name": randomBucketName,
+			"domain_name":      domainName,
+			"name":             bucketName,
+			"namespace":        "gh",
+			"stage":            "dev",
+			"route_53_zone_id": route53ZoneID,
 		},
 		// Disable colors in Terraform commands so its easier to parse stdout/stderr
 		NoColor: true,
@@ -25,7 +39,7 @@ func TestReviewAppResources(t *testing.T) {
 	defer terraform.Destroy(t, terraformOptions)
 	terraform.InitAndApply(t, terraformOptions)
 
-	expectedBucketName := randomBucketName
+	expectedBucketName := bucketName
 	actualBucketName := terraform.Output(t, terraformOptions, "aws_s3_bucket_name")
 	assert.Equal(t, expectedBucketName, actualBucketName)
 }
