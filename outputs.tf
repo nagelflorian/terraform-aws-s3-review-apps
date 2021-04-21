b1d8cb8cec82f2c6723b7a6557bc38e11247818b
Florian Nagel <nagelflorian@users.noreply.github.com>
2021-04-22 01:00:06 +0200

    Update to Support Terraform v0.13 (#9)

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "aws_s3_bucket_name" {
   description = "AWS S3 Bucket Name"
-  value       = "${aws_s3_bucket.default[0].id}"
+  value       = aws_s3_bucket.default[0].id
 }
 
 output "aws_s3_bucket_arn" {
   description = "AWS S3 Bucket ARN"
-  value       = "${aws_s3_bucket.default[0].arn}"
+  value       = aws_s3_bucket.default[0].arn
 }
